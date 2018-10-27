10c9d503ae482b105c34fbc6ee7dd8a43a5be41f
pemil <p.emilia13@yahoo.com>
2018-10-27 14:58:44 +0300

    added login functionality

--- app/src/main/java/com/example/pemil/smarthack/MainActivity.kt
+++ app/src/main/java/com/example/pemil/smarthack/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.example.pemil.smarthack
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var mFireBaseAuth: FirebaseAuth? = null
+    private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
+
+    companion object {
+        const val RC_SIGN_IN = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFireBaseAuth = FirebaseAuth.getInstance()
+
+        mAuthStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                //user is signed in
+            } else {
+                //user is signed out
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(
+                            Arrays.asList<AuthUI.IdpConfig>(
+                                AuthUI.IdpConfig.GoogleBuilder().build(),
+                                AuthUI.IdpConfig.EmailBuilder().build()
+                            )
+                        )
+                        .build(),
+                    RC_SIGN_IN
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFireBaseAuth?.addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mFireBaseAuth != null) {
+            mFireBaseAuth?.removeAuthStateListener(mAuthStateListener)
+        }
     }
 }
