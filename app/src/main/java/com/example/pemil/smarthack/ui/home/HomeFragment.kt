18a83b30a42fdaf11e61c25420f62b85853dca98
Andreea Dima <dimaandreea96@yahoo.com>
2018-10-28 10:46:56 +0200

    Final commit

--- app/src/main/java/com/example/pemil/smarthack/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pemil/smarthack/ui/home/HomeFragment.kt
@@ -30,16 +30,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO - de luat lista de investitii si calculat pentru toate profit
-        all_money_graph.title = "All investments from last 30 days"
-        val point_data = PointsGraphSeries<DataPoint>(arrayOf(DataPoint(0.0, 1.0), DataPoint(1.0, 5.0), DataPoint(2.0, 3.0), DataPoint(3.0, 2.0), DataPoint(4.0, 6.0)))
-        val line_data = LineGraphSeries<DataPoint>(arrayOf(DataPoint(0.0, 1.0), DataPoint(1.0, 5.0), DataPoint(2.0, 3.0), DataPoint(3.0, 2.0), DataPoint(4.0, 6.0)))
-        line_data.setAnimated(true)
-        point_data.setOnDataPointTapListener { _, dataPoint ->
-            Toast.makeText(this.context, dataPoint.toString(), Toast.LENGTH_SHORT).show()
-        }
-        all_money_graph.addSeries(point_data)
-        all_money_graph.addSeries(line_data)
 
         val chart = barchart as BarChart
 
