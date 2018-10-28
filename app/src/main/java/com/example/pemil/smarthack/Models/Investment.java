365af1a8504b09a093e9fe1fa372bdbb94a14aa5
atotputerNICA <nicadanamaria@gmail.com>
2018-10-28 05:48:56 +0200

    investment

--- app/src/main/java/com/example/pemil/smarthack/Models/Investment.java
+++ app/src/main/java/com/example/pemil/smarthack/Models/Investment.java
@@ -8,18 +8,32 @@ public class Investment {
     private String name;
     private String date;
     private String amt;
-    private String category;
-    private String subCategory;
-    private String profit;
+    private Double close;
+    private Double high;
+    private String industry;
+    private Double low;
+    private Double open;
+    private Double predictedValue;
+    private String sector;
+    private Double volum;
 
-    public Investment(String name, String date, String amt, String category, String subCategory, String profit) {
+    public Investment(String name, String date,
+                      String amt, Double close, Double high,
+                      String industry, Double low, Double open,
+                      Double predictedValue, String sector, Double volum) {
         this.name = name;
         this.date = date;
         this.amt = amt;
-        this.category = category;
-        this.subCategory = subCategory;
-        this.profit = profit;
+        this.close = close;
+        this.high = high;
+        this.industry = industry;
+        this.low = low;
+        this.open = open;
+        this.predictedValue = predictedValue;
+        this.sector = sector;
+        this.volum = volum;
     }
+
     public Investment() {}
 
     public String getName() {
@@ -46,28 +60,68 @@ public class Investment {
         this.amt = amt;
     }
 
-    public String getCategory() {
-        return category;
+    public Double getClose() {
+        return close;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setClose(Double close) {
+        this.close = close;
     }
 
-    public String getSubCategory() {
-        return subCategory;
+    public Double getHigh() {
+        return high;
     }
 
-    public void setSubCategory(String subCategory) {
-        this.subCategory = subCategory;
+    public void setHigh(Double high) {
+        this.high = high;
     }
 
-    public String getProfit() {
-        return profit;
+    public String getIndustry() {
+        return industry;
     }
 
-    public void setProfit(String profit) {
-        this.profit = profit;
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public Double getLow() {
+        return low;
+    }
+
+    public void setLow(Double low) {
+        this.low = low;
+    }
+
+    public Double getOpen() {
+        return open;
+    }
+
+    public void setOpen(Double open) {
+        this.open = open;
+    }
+
+    public Double getPredictedValue() {
+        return predictedValue;
+    }
+
+    public void setPredictedValue(Double predictedValue) {
+        this.predictedValue = predictedValue;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public Double getVolum() {
+        return volum;
+    }
+
+    public void setVolum(Double volum) {
+        this.volum = volum;
     }
 
     @Override
@@ -76,9 +130,14 @@ public class Investment {
                 "name='" + name + '\'' +
                 ", date='" + date + '\'' +
                 ", amt='" + amt + '\'' +
-                ", category='" + category + '\'' +
-                ", subCategory='" + subCategory + '\'' +
-                ", profit='" + profit + '\'' +
+                ", close=" + close +
+                ", high=" + high +
+                ", industry='" + industry + '\'' +
+                ", low=" + low +
+                ", open=" + open +
+                ", predictedValue=" + predictedValue +
+                ", sector='" + sector + '\'' +
+                ", volum=" + volum +
                 '}';
     }
 }
